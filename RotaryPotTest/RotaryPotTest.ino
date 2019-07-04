73cbb15c645203d953cc60607fba35ed8829ed3c
Alison <alisonnnw@gmail.com>
2019-07-04 10:58:16 -0400

    Generic Code for testing Potentiometers

---
+++ RotaryPotTest/RotaryPotTest.ino
@@ -0,0 +1,19 @@
+const int sP1 = A0;
+int RawVal1 = 0, onDelay = 50;
+float vVal1 = 0;
+float res1 = 0;
+
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(sP1, INPUT);
+}
+
+void loop() {
+  RawVal1 = analogRead(sP1);
+  vVal1 = (RawVal1 * 5.0) / 1024.0; 
+  res1 = 5000.0 * (RawVal1 / 2024.0);
+  Serial.print("Raw Value 1 = ");
+  Serial.println(RawVal1);
+
+}
