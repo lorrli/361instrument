1422f4e54e3b588884ffd1a9e128b965c5e40305
Alison <alisonnnw@gmail.com>
2019-07-18 09:59:54 -0400

    Code for Volume Setting

--- new_force_sensor_velocity/new_force_sensor_velocity.ino
+++ new_force_sensor_velocity/new_force_sensor_velocity.ino
@@ -1,4 +1,5 @@
 int SensorPin = A16; //analog pin 0
+int velocity[] = {50, 90, 127};
 
 void setup(){
   Serial.begin(9600);
@@ -8,9 +9,12 @@ void setup(){
 
 void loop(){
   int SensorReading = analogRead(SensorPin); 
-//  int mfsr_r18 = map(SensorReading, 0, 1024, 0, 127);
-//  Serial.println(mfsr_r18);
-Serial.println(SensorReading);
-  delay(100); 
+  int mfsr_r18 = map(SensorReading, 0, 900, 0, 2);
+  Serial.println(mfsr_r18);
 
+  Serial.println(SensorReading);
+  usbMIDI.sendNoteOn(60,velocity[mfsr_r18],1); 
+  delay(100); 
+  usbMIDI.sendNoteOff(60,velocity[mfsr_r18],1); 
+  delay(100); 
 }
