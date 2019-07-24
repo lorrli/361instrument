f34600f707f5602ebd14b8556741f696b42fec57
Alison <alisonnnw@gmail.com>
2019-07-23 22:41:06 -0400

    ascending chords

--- guitarchords/guitarchords.ino
+++ guitarchords/guitarchords.ino
@@ -5,7 +5,7 @@
 // Make sure to program your Teensy 3.2 in MIDI mode
 
 // You need to include two libraries:
-// the I2C "Wire.h" library, 
+// the I2C "Wire.h" library,
 // the Adafruit MPR121 Cap-touch Board "Adafruit_MPR121.h" library
 
 #include <Wire.h>
@@ -54,43 +54,50 @@ int vel = 120;
 const int VolumePin = A16;
 const int sP1 = A15;
 const int openNote = 0;
-int velocity[] = {50, 90, 127}; // three volume ranges 
+int velocity[] = {50, 90, 127}; // three volume ranges
 int volumeIndex = 0;
-int SensorReading = 0; 
+int SensorReading = 0;
 int currNote = openNote;
 const int distance = 50;
 int RawVal1, notestep = 0;
-const int cycle=50;
-const int intervals[]={0,4,7,12,16,19};
+const int cycle = 50;
+const int intervals[] = {0, 4, 7, 12, 16, 19};
 /////////chord note stuff
-int chordType = 1;
-int chords[][12][6] = 
-               //major
-               {{{40,48,52,55,60, 64},
-               {41,49,53,56,61,65},
-               {45,50,57,62,66,69},
-               {46,51,58,63,67,70},
-               {40,47,52,56,59,64},
-               {41,48,53,57,60,65},
-               {42,49,54,58,61,66},
-               {43,47,50,55,59,66},
-               {44,51,56,60,63,68},
-               {40,45,52,57,61,64},
-               {42,47,54,59,63,66},
-               {41,46,53,58,62,65}},
-               //minor
-               {{43,48,55,60,63,67},
-               {44,49,56,61,64,68},
-               {45,50,57,62,65,69},
-               {46,51,58,63,66,70},
-               {40,47,52,55,59,64},
-               {41,48,53,56,60,65},
-               {42,49,54,57,61,66},
-               {43,50,55,58,62,67},
-               {44,51,56,59,63,68},
-               {40,45,52,57,60,64},
-               {41,46,53,58,61,65}, 
-               {42,47,54,59,62,66}}};
+int chordType = 0;
+int chords[][12][6] =
+  //major
+{ {
+    {60, 48, 52, 55, 64, 40}, //c
+    {61, 49, 53, 56, 65, 41}, //db
+    {62, 50, 57, 62, 66, 45}, //d
+    {63, 51, 58, 63, 70, 46}, //eb
+    {64, 52, 40, 56, 59, 47}, //e
+    {65, 53, 41, 57, 60, 48}, //f
+    {66, 54, 58, 61, 66, 49}, //f#
+    {67, 55, 50, 59, 47, 50}, //g
+    //    43, 47, 49, 55, 59, 67
+    //    g2, b2, d3, g3, b3, g4
+    {68, 56, 51, 60, 63, 51}, //ab
+    {69, 57, 40, 45, 61, 64}, //a
+    {70, 58, 62, 46, 65, 53}, //Bb
+    {71, 42, 47, 63, 66, 54}  //b
+
+  },
+  //minor
+  { {43, 48, 55, 60, 63, 67},
+    {44, 49, 56, 61, 64, 68},
+    {45, 50, 57, 62, 65, 69},
+    {46, 51, 58, 63, 66, 70},
+    {40, 47, 52, 55, 59, 64},
+    {41, 48, 53, 56, 60, 65},
+    {42, 49, 54, 57, 61, 66},
+    {43, 50, 55, 58, 62, 67},
+    {44, 51, 56, 59, 63, 68},
+    {40, 45, 52, 57, 60, 64},
+    {41, 46, 53, 58, 61, 65},
+    {42, 47, 54, 59, 62, 66}
+  }
+};
 // Setup loop that intitiates the I2C hardware and the hardware MIDI
 void setup() {
   Serial.begin(9600);
@@ -104,7 +111,7 @@ void setup() {
   pinMode(sP1, INPUT);
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
-  
+
 }
 
 // the main loop
@@ -120,71 +127,71 @@ void loop() {
 }
 
 // Function to check the cap-touch board and send turn on/off notes as needed
-void checkCap(){
+void checkCap() {
   //Serial.println("checkCap");
   // Loop through the 12 touch points on the board
   // We use pin 0 for cap touch CC later, so skip that one and start at n = 1
-  for(int n = 1; n <7; n++){
-  
+  for (int n = 1; n < 7; n++) {
+
     // Compare the current state to the previous state
     // If it has changed you need to do something
-    if(bitRead(currstateA,n) ==1 && bitRead(prevstateA,n) != 1){
-      
+    if (bitRead(currstateA, n) == 1 && bitRead(prevstateA, n) != 1) {
+
       // If it has changed and is TRUE, then turn on that note
-     
-        // Send out a MIDI message on both usbMIDI and hardware MIDI ports
-        //chords[0-major,1-minor][base chord note][stringpressed]
-        usbMIDI.sendNoteOn(chords[chordType][11 - currNote][n-1],velocity[volumeIndex],channel);
-        Serial.print("playing:"); 
-        Serial.println(chords[chordType][11 - currNote][n-1]);
+
+      // Send out a MIDI message on both usbMIDI and hardware MIDI ports
+      //chords[0-major,1-minor][base chord note][stringpressed]
+      usbMIDI.sendNoteOn(chords[chordType][currNote][n - 1], velocity[volumeIndex], channel);
+      Serial.print("playing:");
+      Serial.println(chords[chordType][currNote][n - 1]);
 
     }
   }
   delay(cycle);
-  for(int n = 1; n <7; n++){
-  
+  for (int n = 1; n < 7; n++) {
+
     // Compare the current state to the previous state
     // If it has changed you need to do something
-      
-      // If it has changed and is TRUE, then turn on that note
-     
-        // Send out a MIDI message on both usbMIDI and hardware MIDI ports
-        //chords[0-major,1-minor][base chord note][stringpressed]
-        usbMIDI.sendNoteOff(chords[chordType][11 - currNote][n-1],0,channel);
+
+    // If it has changed and is TRUE, then turn on that note
+
+    // Send out a MIDI message on both usbMIDI and hardware MIDI ports
+    //chords[0-major,1-minor][base chord note][stringpressed]
+    usbMIDI.sendNoteOff(chords[chordType][currNote][n - 1], 0, channel);
   }
 }
 
 
-void getNote(){
+void getNote() {
   //read linear pot for note value
   RawVal1 = analogRead(sP1);
-  SensorReading = analogRead(VolumePin); 
+  SensorReading = analogRead(VolumePin);
   volumeIndex = map(SensorReading, 0, 900, 0, 2);
   Serial.print("Volume: ");
   Serial.println(volumeIndex);
   Serial.println(RawVal1);
-  if (RawVal1 >=200 && RawVal1 <=800){
-//    notestep = (RawVal1 - 100)/distance;
-//    currNote = openNote + notestep;
-    currNote = constrain(map(RawVal1, 200, 800, 1, 11),1,11);
-  }else{
+  if (RawVal1 >= 200 && RawVal1 <= 800) {
+    //    notestep = (RawVal1 - 100)/distance;
+    //    currNote = openNote + notestep;
+    currNote = constrain(map(RawVal1, 200, 800, 1, 11), 1, 11);
+  } else {
     currNote = openNote;
   }
   Serial.print(currNote);
 }
 /*********************************************************
-This is a library for the MPR121 12-channel Capacitive touch sensor
+  This is a library for the MPR121 12-channel Capacitive touch sensor
 
-Designed specifically to work with the MPR121 Breakout in the Adafruit shop 
+  Designed specifically to work with the MPR121 Breakout in the Adafruit shop
   ----> https://www.adafruit.com/products/
 
-These sensors use I2C communicate, at least 2 pins are required 
-to interface
+  These sensors use I2C communicate, at least 2 pins are required
+  to interface
 
-Adafruit invests time and resources providing this open source code, 
-please support Adafruit and open-source hardware by purchasing 
-products from Adafruit!
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
 
-Written by Limor Fried/Ladyada for Adafruit Industries.  
-BSD license, all text above must be included in any redistribution
+  Written by Limor Fried/Ladyada for Adafruit Industries.
+  BSD license, all text above must be included in any redistribution
 **********************************************************/
